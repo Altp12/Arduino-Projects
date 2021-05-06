dec22610171877bac297bcba8c9b16853ddb82fb
Alief Gilang <aliefgilang54@gmail.com>
2021-05-06 21:23:45 +0800

    update I2C-LCD-Display.ino

--- I2C_LCD-Display/I2C_LCD-Display.ino
+++ I2C_LCD-Display/I2C_LCD-Display.ino
@@ -6,16 +6,15 @@
 #include<LiquidCrystal_I2C.h>
 #include<Wire.h>
 
-LiquidCrystal_I2C(0x27,20,4);  //Assume the address of LCD is 0x27 and the size of LCD is 20x4, you can adjust it by yourself
+LiquidCrystal_I2C lcd(0x27, 20, 4);  //Assume the address of LCD is 0x27 and the size of LCD is 20x4, you can adjust it by yourself
    
 void setup() {
   Serial.begin(9600);
   lcd.init(); //LCD initialisation
   lcd.backlight();  //Turn on LCD backlight
   lcd.setCursor(0,0); //Set cursor to position (0,0)
-  lcd.print("Hello World!");  //You can adjust the text to be printed to LCD
+  lcd.print("Hello, World!");  //You can adjust the text to be printed to LCD
 }
 
 void loop() {
-  
 }
